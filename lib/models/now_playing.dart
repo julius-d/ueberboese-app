9f0688fe2eb772e0ed6c81fcc7045b70d9f8d9b8
Julius <30121440+julius-d@users.noreply.github.com>
2025-12-21 23:41:52 +0100

    feat: add now playing

---
+++ lib/models/now_playing.dart
@@ -0,0 +1,21 @@
+class NowPlaying {
+  final String? track;
+  final String? artist;
+  final String? album;
+  final String? art;
+  final String? artImageStatus;
+  final String? shuffleSetting;
+  final String? repeatSetting;
+  final String? playStatus;
+
+  const NowPlaying({
+    this.track,
+    this.artist,
+    this.album,
+    this.art,
+    this.artImageStatus,
+    this.shuffleSetting,
+    this.repeatSetting,
+    this.playStatus,
+  });
+}
